e19c0aad8824a506022d16211ea50eea5aa9fcb7
Divij <divij.edcellpup@gmail.com>
2019-03-29 17:02:32 +0530

    Profile Fragment Updated

--- app/src/main/java/com/example/freshup/Activities/NavigatorActivity.java
+++ app/src/main/java/com/example/freshup/Activities/NavigatorActivity.java
@@ -1,5 +1,6 @@
 package com.example.freshup.Activities;
 
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
@@ -10,6 +11,7 @@ import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RelativeLayout;
 
 import com.example.freshup.Fragments.HomeFragment;
 import com.example.freshup.Fragments.ProductsFragment;
@@ -27,6 +29,7 @@ public class NavigatorActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_navigator);
+
 
 
         SelectFragment(new HomeFragment());
--- app/src/main/java/com/example/freshup/Fragments/ProfileFragment.java
+++ app/src/main/java/com/example/freshup/Fragments/ProfileFragment.java
@@ -1,12 +1,18 @@
 package com.example.freshup.Fragments;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
 
+import com.example.freshup.Activities.LoginActivity;
+import com.example.freshup.Activities.NavigatorActivity;
 import com.example.freshup.R;
 
 
@@ -14,6 +20,9 @@ import com.example.freshup.R;
  * A simple {@link Fragment} subclass.
  */
 public class ProfileFragment extends Fragment {
+    ImageView edit;
+    RelativeLayout logout;
+    EditText name,email,number;
 
 
     public ProfileFragment() {
@@ -24,8 +33,32 @@ public class ProfileFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        View view=inflater.inflate(R.layout.fragment_profile, container, false);
+        edit=view.findViewById(R.id.edit);
+        logout=view.findViewById(R.id.logout_button);
+        name=view.findViewById(R.id.name_et);
+        email=view.findViewById(R.id.emal_et);
+        number=view.findViewById(R.id.number_et);
+
+
+        edit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                name.setFocusable(true);
+                email.setFocusable(true);
+                number.setFocusable(true);
+            }
+        });
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent =new Intent(getActivity(),LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+        return view;
     }
 
 }
