dcf36ca266283125ba57f53c0eec18cb8218aa63
Divij <divij.edcellpup@gmail.com>
2019-04-08 16:03:43 +0530

    Book Slot Activity

---
+++ app/src/main/java/com/example/freshup/Activities/BookSlotActivity.java
@@ -0,0 +1,53 @@
+package com.example.freshup.Activities;
+
+import android.os.Build;
+import android.support.annotation.RequiresApi;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.freshup.R;
+
+import org.joda.time.DateTime;
+
+import java.text.DateFormatSymbols;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import noman.weekcalendar.WeekCalendar;
+import noman.weekcalendar.listener.OnDateClickListener;
+
+public class BookSlotActivity extends AppCompatActivity {
+
+    WeekCalendar weekCalendar;
+    TextView getDate;
+    ImageView back;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_book_slot);
+        weekCalendar = (WeekCalendar)findViewById(R.id.weekCalendar);
+        getDate = (TextView)findViewById(R.id.getDate);
+
+        final String date = new SimpleDateFormat("EEEE MMMMM dd, yyyy", Locale.getDefault()).format(new Date());
+        getDate.setText(""+date);
+
+        weekCalendar.setOnDateClickListener(new OnDateClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.N)
+            @Override
+            public void onDateClick(DateTime dateTime) {
+                String[] weekdays = new DateFormatSymbols().getWeekdays();
+                String[] month = new DateFormatSymbols().getMonths();
+                String chosen_date = weekdays[dateTime.getDayOfWeek()]+" "+month[dateTime.getMonthOfYear()]+
+                        ""+dateTime.getDayOfMonth()+","+dateTime.getYear();
+
+                getDate.setText(chosen_date);
+            }
+        });
+
+
+    }
+}
--- app/src/main/java/com/example/freshup/Activities/PresentationsActivity.java
+++ app/src/main/java/com/example/freshup/Activities/PresentationsActivity.java
@@ -1,9 +1,12 @@
 package com.example.freshup.Activities;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.freshup.Adapters.ServicesTypeAdapter;
 import com.example.freshup.R;
@@ -14,16 +17,25 @@ import java.util.List;
 public class PresentationsActivity extends AppCompatActivity {
     RecyclerView services_type,services_expanded;
     List<String> list=new ArrayList<>();
+    Button Continue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_presentations);
         services_type=findViewById(R.id.services_type);
+        Continue=findViewById(R.id.continue_presentations);
         list.add("COUPES");
         list.add("COLORATION");
         services_type.setLayoutManager(new LinearLayoutManager(this));
         services_type.setAdapter(new ServicesTypeAdapter(this,list));
+        Continue.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(PresentationsActivity.this,BookSlotActivity.class);
+                startActivity(intent);
+            }
+        });
 
 
     }
