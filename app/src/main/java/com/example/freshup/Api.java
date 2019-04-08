381441fa284357678b5be80ce27b7f9b6993f4c9
Divij <divij.edcellpup@gmail.com>
2019-04-08 16:51:14 +0530

    Retrofit introduced

---
+++ app/src/main/java/com/example/freshup/Api.java
@@ -0,0 +1,20 @@
+package com.example.freshup;
+
+import com.example.freshup.Models.RegisterModel;
+
+import retrofit2.Call;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.POST;
+
+public interface Api {
+    @FormUrlEncoded
+    @POST("userRegister")
+    Call<RegisterModel> UserRegister(@Field("name") String name,
+                                     @Field("email") String email,
+                                     @Field("phone") String phone,
+                                     @Field("password") String password,
+                                     @Field("device_type") String device_type,
+                                     @Field("reg_id") String reg_id);
+
+}
---
+++ app/src/main/java/com/example/freshup/Models/RegisterModel.java
@@ -0,0 +1,4 @@
+package com.example.freshup.Models;
+
+public class RegisterModel {
+}
