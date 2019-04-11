978085f4d43feb9afa20c5b3cfc81513e2e1ee76
Divij <divij.edcellpup@gmail.com>
2019-04-12 00:38:13 +0530

    init

---
+++ app/src/main/java/com/example/freshup/Models/GetServicesDataModel.java
@@ -0,0 +1,29 @@
+package com.example.freshup.Models;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class GetServicesDataModel {
+    @SerializedName("message")
+    @Expose
+    private String message;
+    @SerializedName("success")
+    @Expose
+    private String success;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getSuccess() {
+        return success;
+    }
+
+    public void setSuccess(String success) {
+        this.success = success;
+    }
+}
